520ee18f44a0aff29f1c9f38e2267617c82e3ab4
Ignacio Vizzo <ignaciovizzo@gmail.com>
2019-04-11 16:25:58 +0200

    Change #pragma once for headear guard

--- src/sifts.hpp
+++ src/sifts.hpp
@@ -3,10 +3,14 @@
 // @maintainer Ignacio Vizzo     [ivizzo@uni-bonn.de]
 //
 // Copyright (c) 2018 Igor Bogoslavskyi, all rights reserved
-#pragma once
+#ifndef SIFTS_HPP_
+#define SIFTS_HPP_
+
 #include <string>
 
 #include <opencv2/core/core.hpp>
 
 cv::Mat computeSifts(const std::string& fileName,
                      cv::InputOutputArray imageWithKeypoints);
+
+#endif  // SIFTS_HPP_
