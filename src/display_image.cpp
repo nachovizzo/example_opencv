801ff59c97d11b5d2b7add3a91c72660c9d10604
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-05-21 12:02:19 -0300

    Add missing namespace

--- src/display_image.cpp
+++ src/display_image.cpp
@@ -17,7 +17,7 @@ int main() {
     return -1;
   }
   cv::namedWindow("Window Name", cv::WINDOW_AUTOSIZE);
-  imshow("Window Name", image);
+  cv::imshow("Window Name", image);
 
   cv::waitKey(0);
 
