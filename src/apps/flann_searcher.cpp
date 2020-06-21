83dfcbeb16a97f3439486c234be68a1192802fd4
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-06-21 01:08:33 -0300

    Revert "Simplify a bit FLANN example"
    This might be even more confusing, specially when trying to use KDTrees
    as data members of a class.
    
    This reverts commit f6704df7c8c58df3ac7c9d69d6b33f1bb0d6e5e2.

--- src/apps/flann_searcher.cpp
+++ src/apps/flann_searcher.cpp
@@ -6,10 +6,11 @@
 #include <iostream>
 
 #include <opencv2/core.hpp>
-#include <opencv2/flann.hpp>
+#include <opencv2/xfeatures2d.hpp>
 
 using cv::DataType;
 using cv::Mat;
+using cv::Scalar;
 using std::cout;
 using std::endl;
 
@@ -23,38 +24,37 @@ int main() {
   int step = 9;
   int max_data_idx = 100;
   for (int i = 0; i < max_data_idx; i += step) {
-    Mat vec(rows_num, cols_num, DataType<float>::type, static_cast<float>(i));
+    Mat vec(rows_num, cols_num, DataType<float>::type, static_cast<Scalar>(i));
     data.push_back(vec);
   }
   cout << "whole data is \n" << data << endl << endl;
 
   // create a kdtree for searching the data
-  cvflann::KDTreeIndexParams index_params;
-  cv::flann::GenericIndex<cvflann::L2<float>> kdtree(data, index_params);
+  cv::flann::KDTreeIndexParams index_params(4);
+  cv::flann::Index kdtree(data, index_params);
 
   // create a query vector
   int query_idx = 50;
   Mat query(rows_num, cols_num, DataType<float>::type,
-            static_cast<float>(query_idx));
+            static_cast<Scalar>(query_idx));
   cout << "query vector is \n" << query << endl << endl;
 
   // search the nearest vector to it
-  int knn = 2;
-  std::vector<int> indices(knn);
-  std::vector<float> distances(knn);
-  cvflann::SearchParams search_params;
-  kdtree.knnSearch(query, indices, distances, knn, search_params);
+  int k = 2;
+  Mat nearest_vector_idx(1, k, DataType<int>::type);
+  Mat nearest_vector_dist(1, k, DataType<float>::type);
+  kdtree.knnSearch(query, nearest_vector_idx, nearest_vector_dist, k);
 
-  int closest_idx = indices[0];
-  float closest_dist = distances[0];
+  int closest_idx = nearest_vector_idx.at<int>(0, 0);
+  float closest_dist = nearest_vector_dist.at<float>(0, 0);
   cout << "Closest idx = " << closest_idx
        << ", with distance = " << closest_dist << endl;
   cout << "Closest vector is data[" << closest_idx << "]\n"
        << data.row(closest_idx) << endl
        << endl;
 
-  int second_closest_idx = indices[1];
-  float second_closest_dist = distances[1];
+  int second_closest_idx = nearest_vector_idx.at<int>(0, 1);
+  float second_closest_dist = nearest_vector_dist.at<float>(0, 1);
   cout << "Second closest idx = " << second_closest_idx
        << ", with distance = " << second_closest_dist << endl;
   cout << "Second closest vector is data[" << second_closest_idx << "]\n"
